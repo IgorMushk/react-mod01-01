8e8d17a021655b6648da23625e284e920760c121
IgorMushk <igor.mushketik@gmail.com>
2023-08-29 21:24:38 +0300

    work with DOM in JS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+//  in JS
+const div = document.createElement("div");
+
+const root = document.getElementById("root");
+//console.log(root);
+root.append(div);
+//
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<React.StrictMode></React.StrictMode>);
