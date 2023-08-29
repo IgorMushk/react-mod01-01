47d18c4dfbddc28775bbf23e1bdae2e09dd62399
IgorMushk <igor.mushketik@gmail.com>
2023-08-29 22:36:08 +0300

    React.createElement

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
+import React from "react";
+//import ReactDOM from "react-dom"; // ver.17
+import ReactDOM from "react-dom/client"; // vewr.18
 // import "./index.css";
 
-//  in JS
-const div = document.createElement("div");
+// //  in JS
+// const div = document.createElement("div");
 
-const root = document.getElementById("root");
-//console.log(root);
-root.append(div);
-//
+//const root = document.getElementById("root"); // JS & React
+// //console.log(root);
+// root.append(div);
+// //
+
+// in React
+const div = React.createElement("div");
+
+console.log("div :>> ", div);
+
+//ReactDOM.render(div, root); // ver.17
+//ReactDOM.createRoot(root).render(div); // ver/18
+ReactDOM.createRoot(document.getElementById("root")).render(div); // ver/18
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<React.StrictMode></React.StrictMode>);
