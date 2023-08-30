d81465314af6be922a63bbae0e7525a20b48f1ba
IgorMushk <igor.mushketik@gmail.com>
2023-08-30 12:55:09 +0300

    Add 2- component

--- src/index.js
+++ src/index.js
@@ -48,6 +48,10 @@ import data from "./data.json";
 //- );
 //-console.log("div :>> ", div);
 
+const Head = () => {
+  return <p>I am Head</p>;
+};
+
 // fom Bootstap (https://getbootstrap.com/docs/5.3/components/card/)
 const Card = () => {
   return data.map((photo) => {
@@ -56,9 +60,11 @@ const Card = () => {
         <img src={photo.url} className="card-img-top" alt={photo.title} />
         <div className="card-body">
           <h5 className="card-title">Card title: {photo.title}</h5>
-          <p className="card-text">
+          {/* <p className="card-text">
             Some quick example text to build on the card title and make up the bulk of the card's content.
-          </p>
+          </p> */}
+          {/* {head()} */}
+          <Head />
           <a href="#" class="btn btn-primary">
             Go somewhere
           </a>
