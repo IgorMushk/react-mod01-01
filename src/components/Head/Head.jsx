72cb656e7a3dd861a702f0c32ac394f9d0dc1c56
IgorMushk <igor.mushketik@gmail.com>
2023-08-30 20:41:31 +0300

    React.Fragment

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -1,11 +1,13 @@
+import React from "react";
+
 export const Head = ({ clas, id, data, children }) => {
   console.log("children :>>", children);
   return (
-    <>
+    <React.Fragment>
       {children}
       <p id={id} className={clas}>
         I am Head
       </p>
-    </>
+    </React.Fragment>
   );
 };
